d31c0d0da00114f18db4b4a72f10dec4f62e181e
Ray20140121 <rayzhao140121@gmail.com>
2025-12-27 16:11:54 -0800

    updated code

--- src/main.cpp
+++ src/main.cpp
@@ -2,19 +2,23 @@
 #include <vector>
 #include <string>
 
+pros::Controller master(pros::E_CONTROLLER_MASTER);
 Motor leftFront(-11);
-Motor leftBottom(-13);
-Motor leftTop(14);
-Motor rightFront(17);
-Motor rightBottom(18);
-Motor rightTop(-20);
-Motor intake(5);
-Motor score(-10);
+Motor leftBottom(-12);
+Motor leftTop(13);
+Motor rightFront(20);
+Motor rightBottom(19);
+Motor rightTop(-18);
+
+MotorGroup leftMg({-11,-12,13}); 
+MotorGroup rightMg({20,19,-18}); 
+Motor intake(-1);
+Motor score(9);
 adi::Pneumatics lift('a',false);
 adi::Pneumatics loader('b',false);
-Imu imu(9);
+Imu imu(21);
 void powerDriveMotor(int powerForward,int powerTurn);
-void driveForward(int distance, int timeout = 1500,int speed = 80);
+void driveForward(double distance, int timeout,int speed, double Kp, double Kd, double TurningKp);
 void turn(int turn);
 int speedLimit(int speed,int limit);
 
@@ -32,8 +36,7 @@ int current_auton = 0;
 
 void initialize() {
 	lcd::initialize();
-	lcd::set_text(1, "Hello PROS User!");
-	lcd::register_btn1_cb(on_center_button);
+	imu.reset();
 }
 
 void disabled() {}
@@ -41,40 +44,85 @@ void disabled() {}
 void competition_initialize() {}
 
 void autonomous() {
+	leftMg.set_brake_mode(MOTOR_BRAKE_COAST);
+	rightMg.set_brake_mode(MOTOR_BRAKE_COAST);
+
+	driveForward(1165,2000,65,5,0.8,5); //this is for driving forward to balls
 	intake.move (127);
-	delay(200);
-	driveForward(650,2800,40);
-	//first three balls
-	delay(1000);
-	turn(123);
-	delay(500);
-	loader.extend();
-	delay(200);
-	leftBottom.tare_position(); // resetting the encoder that is being used 
-	driveForward(400,1500,60);
-	turn(45);
-	delay(200);
-	driveForward(500);
-	//matchload
-	delay(1000);
-	leftBottom.tare_position(); // resetting the encoder that is being used 
-	driveForward(-700,-80);
-	loader.retract();
-	delay(200);
+	turn(90);
+	driveForward(700,1500,20,1.4,3,5);//intake 3 balls
+	delay(800);
+	turn(90);
+	driveForward(500,1200,60,3,0.5,5);//getting to the goal
+	turn(-88);
+	driveForward(1350,2000,60,3,2.2,5);//getting to the goal
+	turn(91);
 	lift.extend();
 	delay(200);
-	driveForward(-1300, -80);
-	//score
+	driveForward(-500,1000,40,1,2,5);//line up with goal
+	delay(300);
+	score.move(127);//score!!!!!
+	delay(800);
+	score.move(-127);
+	delay(300);
+	score.move(127);//score!!!!!
+	delay(800);
+	score.move(-127);
+	delay(300);
+	score.move(0);
+	loader.extend();
+	driveForward(1600,1500,70,3,5,5);//Match load
+	delay(800);
+	//driveForward(-1400,1000,70,3,5,5);//score!!!
+	//loader.retract();
+	//score.move(-127);
+	//delay(500);
+	//score.move(127);
+	//delay(100);
+	//score.move(-127);
+	//delay(500);
+	//score.move(127);
+
+	/*Right
+	leftMg.set_brake_mode(MOTOR_BRAKE_COAST);
+	rightMg.set_brake_mode(MOTOR_BRAKE_COAST);
+
+	driveForward(1165,2000,65,5,0.8,5); //this is for driving forward to balls
+	intake.move (127);
+	turn(90);
+	driveForward(700,1500,20,1.4,3,5);//intake 3 balls
+	delay(800);
+	turn(90);
+	driveForward(500,1200,60,3,0.5,5);//getting to the goal
+	turn(-88);
+	driveForward(1350,2000,60,3,3,5);//getting to the goal
+	turn(91);
+	lift.extend();
 	delay(200);
-	score.move_absolute(290,127);
-	delay(500);
-	score.move_absolute(-290,127);
-	delay(500);
-	score.move_absolute(350,127);
+	driveForward(-500,1000,40,1,2,5);//line up with goal
+	delay(300);
+	score.move(127);//score!!!!!
+	delay(800);
+	score.move(-127);
+	delay(300);
+	score.move(127);//score!!!!!
+	delay(800);
+	score.move(-127);
+	delay(300);
+	score.move(0);*/
 }
 void opcontrol() {
-	Controller master(pros::E_CONTROLLER_MASTER);
+	leftMg.tare_position();
+	rightMg.tare_position();
+	score.tare_position();
+
 	while (true) {
+		lcd::print(1, "left: %f", leftFront.get_position()); // diasplay motor encoder
+		//lcd::print(2, "leftTop: %f", leftTop.get_position());
+		lcd::print(2, "right: %f", imu.get_rotation());
+		//lcd::print(4, "rightFront: %f", rightFront.get_position());
+		//lcd::print(5, "rightTop: %f", rightTop.get_position());
+		//lcd::print(6, "rightBottom: %f", rightBottom.get_position());
 		int dir = master.get_analog(ANALOG_LEFT_Y);
 		int turn = master.get_analog(ANALOG_RIGHT_X);
 		powerDriveMotor(dir, turn);
@@ -83,19 +131,19 @@ void opcontrol() {
 			intake.move (127);
 		}
 		else if(master.get_digital(DIGITAL_L2)){
-			intake.move (-127);
+			intake.move(-127);
 		}
 		else{
 			intake.move (0);
 		}
 		if(master.get_digital(DIGITAL_R1)){
-			score.move(-127);
-		}
-		else if(master.get_digital(DIGITAL_R2)){
 			score.move(127);
 		}
+		// else if(master.get_digital(DIGITAL_R2)){
+		// 	score.move(-127);
+		// }
 		else{
-			score.move (0);
+			score.move_absolute(0,200);
 		}
 		if(master.get_digital_new_press(DIGITAL_B)){
 			lift.toggle();
@@ -116,20 +164,27 @@ void powerDriveMotor(int powerForward,int powerTurn){
 	rightTop.move(powerForward - powerTurn);
 }
 
-void driveForward(int distance, int timeout,int speed){
-	leftTop.tare_position();
-	int multiplierConstant = 1; 
-	int power = 1;
-	int drivingDistanceRemaining = distance - leftTop.get_position(); 
+void driveForward(double distance, int timeout,int speed, double Kp, double Kd, double TurningKp){
+	leftMg.tare_position();
+	rightMg.tare_position();
+	imu.tare_rotation();
+	double power;
+	double lastError, derivative;
+	double turningError; 
+	double drivingDistanceRemaining = distance - ((leftMg.get_position() + rightMg.get_position())/2); 
 	int startTime = millis();
-	while(std::abs(drivingDistanceRemaining) >= 3 && millis() - startTime < timeout){ //distance left 
-		drivingDistanceRemaining = distance - leftTop.get_position();
-		power = drivingDistanceRemaining*multiplierConstant; 
-		powerDriveMotor(speedLimit(power,speed),0); 
-		delay(20);
+	while(abs(drivingDistanceRemaining) >= 1 && millis() - startTime < timeout){ //distance left 
+		drivingDistanceRemaining = distance - ((leftMg.get_position() + rightMg.get_position())/2);
+		turningError = imu.get_rotation(); 
+		derivative = lastError - drivingDistanceRemaining; 
+		lastError = drivingDistanceRemaining; 
+		power = drivingDistanceRemaining*Kp + derivative*Kd; 
+		powerDriveMotor(speedLimit(power,speed),-turningError*TurningKp); 
+		delay(8);
 	}
 	powerDriveMotor(0,0);
 }
+
 void turn(int turn){
 	imu.tare_rotation();
 	int multiplierConstant = 1; 
@@ -144,15 +199,17 @@ void turn(int turn){
 	}
 	powerDriveMotor(0,0);
 }
-pros::Controller master(pros::E_CONTROLLER_MASTER);
 
 int speedLimit(int speed, int limit) {
 	if (speed > limit){
 		return limit;
 	}
-	else if(speed < limit){
-		return limit;
+	else if(speed < limit && speed > 0){
+		return speed;
 	} 
+	else if(speed < 0 && abs(speed) > limit){
+		return -limit; 
+	}
 	else {
 		return speed;
 	}
